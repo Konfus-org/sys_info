ab35f4824c89cd6e968807c5b88d734885ebda6c
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-05-28 17:07:09 -0700

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -4,20 +4,6 @@ project "sys_info"
     cppdialect "C++17"
     staticruntime "Off"
 
-    flags
-    {
-        "MultiProcessorCompile"
-    }
-
-    if OutputIntermediateDir == nil or OutputTargetDir == nil then
-        targetdir ("Build/bin/%{prj.name}/")
-        objdir    ("Build/obj/%{prj.name}/")
-
-    else
-        targetdir ("../../" .. OutputTargetDir .. "")
-        objdir    ("../../" .. OutputIntermediateDir .. "")
-    end
-
     files
     {
         "./**.h",
@@ -27,24 +13,7 @@ project "sys_info"
         "./**.md",
         "./**.lua",
     }
-
     includedirs
     {
         "./core/include",
     }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        buildoptions { "/MDd" }
-        symbols "On"
-
-    filter "configurations:Optimized"
-        runtime "Release"
-        buildoptions { "/MDd" }
-        optimize "On"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "On"
-        buildoptions { "/MD" }
-        symbols "Off"
